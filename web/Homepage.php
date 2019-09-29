191753cda55fc3ba8a0828faef509c1964b3ea30
Shean92 <sheanthebond92@gmail.com>
2019-09-28 19:36:57 -0600

    Added php

--- web/Homepage.php
+++ web/Homepage.php
@@ -10,6 +10,14 @@
         <h2>I'm Shean Brunner.</h2>
         <p class="Intro-Text">My wife and I were born in Las Vegas. I love making games through Unity and am trying to get better at game development. I love singing and dancing and art. I like to play D&#38D, Magic The Gathering, and Hearthstone.</p>
         <button class="btn" type="button" onclick="assignmentButton()">My Projects</button>
+        <?php
+        echo "Also I know PHP!";
+        $x = 2;
+        echo "$x <br>";
+        $y = 2;
+        echo "$y <br>";
+        echo "x + y = " . $x+$y .;
+        >
     </div>
     <script type="text/javascript" src="Homepage.js"></script>
 </body>
