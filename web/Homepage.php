99ac00dafe9ffe0d66b6fd442dfea52c13b5b769
Shean92 <sheanthebond92@gmail.com>
2019-09-28 19:06:32 -0600

    Added homepage and assignments pages.

---
+++ web/Homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage of Shean Brunner</title>
+    <link rel="stylesheet" href="Homepage.css">
+</head>
+<body>
+    <div class="Introduction">
+        <img class="MyImage" src="Assets/me.jpg" alt="A picture of me">
+        <h2>I'm Shean Brunner.</h2>
+        <p class="IntroText">My wife and I were born in Las Vegas. I love making games through Unity and am trying to get better at game development. I love singing and dancing and art. I like to play D&#38D, Magic The Gathering, and Hearthstone.</p>
+        <button class="btn" type="button" href="Assignments.html">My Projects</button>
+    </div>
+    <script type="text/javascript" src="Homepage.js"></script>
+</body>
+</html>
